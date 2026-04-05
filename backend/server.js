3959082a56764bba6a586e1b344f0a4c5620cae3
Yalçın Gülşen <yalcingulsen@192.168.1.110>
2026-04-05 23:38:14 +0300

    Render port fix

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 const path = require("path");
 
 const app = express();
-const PORT = 5001;
+const PORT = process.env.PORT || 5001;
 
 app.use(cors());
 app.use(express.json());
